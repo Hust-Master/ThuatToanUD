2f5b83702eb029c76db0a2469236ed439aec2fd2
Doan Ngoc Minh <minhdn@aits.vn>
2025-10-17 11:50:24 +0700

    ref

--- main_12_1.py
+++ main_12_1.py
@@ -1,20 +1,30 @@
-# Chapter 1. 2 - 2pointing1DarrayAsceding
-def count_pairs(arr, Q):
+
+def count_pairs (arr, Q):
+    '''
+    Given the sequence a[1], a[2], . . ., a[n] is sorted in ascending order (distinct elements: no elements with the same value). 
+    Given the value Q, count M as the number of pairs of 2 indices i and j such that a[i] + a[j] = Q.
+    Args:
+        arr (list):  a[1], a[2], . . ., a[n] 
+        Q (int): 1 <= n, Q <= 1000000
+    Returns:
+        M: 
+    '''
     l, r = 0, len(arr) - 1
-    M = 0
+    count = 0
     while l < r:
-        s = arr[l] + arr[r]
-        if s == Q:
-            M += 1
+        currentSum = arr[l] + arr[r]
+
+        if (currentSum == Q):
+            count += 1
             l += 1
             r -= 1
-        elif s < Q:
+        elif (currentSum < Q):
             l += 1
         else:
             r -= 1
-    return M
+    return count
 
 if __name__ == "__main__":
     n, Q = map(int, input().split())
     arr = list(map(int, input().split()))
-    print(count_pairs(arr, Q))
+    print("total pairs:", count_pairs(arr, Q))
